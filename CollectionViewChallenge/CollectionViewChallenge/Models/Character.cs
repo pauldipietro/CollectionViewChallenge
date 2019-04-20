db318e7a2e761b15f20bef174587b3006b2bb9be
Bill <bklinton14@gmail.com>
2019-04-20 09:08:46 -0300

    Initial commit

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionViewChallenge.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+
+        public string Path { get; set; }
+    }
+}
---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Data.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionViewChallenge.Models
+{
+    public class Data
+    {
+        [JsonProperty("results")]
+        public List<Character> Characters { get; set; }
+    }
+}

47cbcc5f560f94ba6174cf26313b176ea762f7ac
Depechie <glenn.versweyveld@gmail.com>
2019-04-26 00:02:24 +0200

    Setup main page Setup tabs Setup first CollectionView

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/StatisticsDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CollectionViewChallenge.Models
+{
+    public class StatisticsDetail
+    {
+        public string DisplayValue { get; set; }
+        public string DisplayDescription { get; set; }
+
+        public string Icon { get; set; }
+    }
+}
--- CollectionViewChallenge/CollectionViewChallenge/Views/CollectionViewChallengePage.xaml.cs
+++ CollectionViewChallenge/CollectionViewChallenge/Views/CollectionViewChallengePage.xaml.cs
@@ -1,20 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using CollectionViewChallenge.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CollectionViewChallenge.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CollectionViewChallengePage : ContentPage
+    public partial class CollectionViewChallengePage : ContentPage, INotifyPropertyChanged
     {
+        private int _selectedViewModelIndex = 0;
+        public int SelectedViewModelIndex
+        {
+            get => _selectedViewModelIndex;
+            set
+            {
+                _selectedViewModelIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<StatisticsDetail> _stats = new ObservableCollection<StatisticsDetail>();
+        public ObservableCollection<StatisticsDetail> Stats
+        {
+            get => _stats;
+            set
+            {
+                _stats = value;
+                OnPropertyChanged();
+            }
+        }
+
         public CollectionViewChallengePage()
         {
             InitializeComponent();
+
+            Stats.Add(new StatisticsDetail()
+            {
+                DisplayValue = "24,5 km",
+                DisplayDescription = "total distance"
+            });
+            Stats.Add(new StatisticsDetail()
+            {
+                DisplayValue = "27,1 km",
+                DisplayDescription = "average speed"
+            });
+
+            BindingContext = this;
         }
     }
 }

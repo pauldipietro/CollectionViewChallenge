ef9cd8b305c9fc44816969d157cd038624e3bb71
Delpin Susai Raj <delpinsusairajm@rajasri.net>
2019-04-25 12:19:04 +0530

    CollectionView Worked

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/ListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CollectionViewChallenge.Models
+{
+    public class ListItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Percentage { get; set; }
+       
+    }
+}

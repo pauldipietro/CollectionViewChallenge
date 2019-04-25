edc4bb6fab48b06abda78b83b0a80316a2d1e1f7
Robson Amorim <robson.amorim@lambda3.com.br>
2019-04-25 16:39:10 -0300

    add ifood collectionviewpage

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Restaurante.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionViewChallenge.Models
+{
+    public class Restaurante
+    {
+        public string Nome { get; private set; }
+        public string Foto { get; private set; }
+
+        public Restaurante(string nome, string foto)
+        {
+            Nome = nome;
+            Foto = foto;
+        }
+    }
+}

f34896ffc42854ade429620512f7f795df4e5a71
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-04-29 15:08:11 +0200

    CollectionViewChallenge - Pokédex by Luis

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Pokemon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using CollectionViewChallenge.Class;
+
+namespace CollectionViewChallenge.Models
+{
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public Name name { get; set; }
+        public List<string> type { get; set; }
+
+        [JsonProperty("base")]
+        public Base _base { get; set; }
+
+        public string ImageURL
+        {
+            get
+            {
+                var num = id.ToString().PadLeft(3, '0');
+                return $"https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/{num}{name.english}.png";
+            }
+        }
+
+        public string Types => string.Join(", ", type);
+
+        public double HP => _base.HP * Constants.factor;
+        public double Attack => _base.Attack * Constants.factor;
+        public double Defense => _base.Defense * Constants.factor;
+        public double SpAttack => _base.SpAttack * Constants.factor;
+        public double SpDefense => _base.SpDefense * Constants.factor;
+        public double Speed => _base.Speed * Constants.factor;
+    }
+
+    public class Name
+    {
+        public string english { get; set; }
+        public string japanese { get; set; }
+        public string chinese { get; set; }
+    }
+
+    public class Base
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+
+        [JsonProperty("Sp. Attack")]
+        public int SpAttack { get; set; }
+
+        [JsonProperty("Sp. Defense")]
+        public int SpDefense { get; set; }
+
+        public int Speed { get; set; }
+    }
+}
---
+++ CollectionViewChallenge/CollectionViewChallenge/Services/PokemonApiService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CollectionViewChallenge.Models;
+using Newtonsoft.Json;
+
+namespace CollectionViewChallenge.Services
+{
+    public static class PokemonApiService
+    {
+        private static readonly HttpClient client = CrearHttpClient();
+
+        private static HttpClient CrearHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+
+        public async static Task<List<Pokemon>> GetPokemon()
+        {
+            var response = await client.GetAsync("https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var list = JsonConvert.DeserializeObject<List<Pokemon>>(json);
+                return list;
+            }
+
+            return default(List<Pokemon>);
+        }
+    }
+}

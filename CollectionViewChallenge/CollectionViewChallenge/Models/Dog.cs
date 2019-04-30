64b2db7bd834f8c40014b2be919571e8114bf31d
tdemeneghi <tdemeneghi@mikuni.com>
2019-05-01 01:47:23 +0200

    CollectionView Challenge - dog list implementation

---
+++ CollectionViewChallenge/CollectionViewChallenge/Models/Dog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CollectionViewChallenge.Models
+{
+    public class Dog
+    {
+        public Breed[] breeds { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public string breed => string.Join(", ", breeds.Select(x => x.name));
+        public string temperament => string.Join(", ", breeds.Select(x => x.temperament));
+    }
+
+    public class Breed
+    {
+        public Weight weight { get; set; }
+        public Height height { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string bred_for { get; set; }
+        public string breed_group { get; set; }
+        public string life_span { get; set; }
+        public string temperament { get; set; }
+        public string origin { get; set; }
+        public string country_code { get; set; }
+    }
+
+    public class Weight
+    {
+        public string imperial { get; set; }
+        public string metric { get; set; }
+    }
+
+    public class Height
+    {
+        public string imperial { get; set; }
+        public string metric { get; set; }
+    }
+
+}
---
+++ CollectionViewChallenge/CollectionViewChallenge/ViewModels/DogViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using CollectionViewChallenge.Models;
+using CollectionViewChallenge.Services;
+using System.Runtime.CompilerServices;
+
+namespace CollectionViewChallenge.ViewModels
+{
+    public class DogViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private List<Dog> dogList;
+
+        private ObservableCollection<Dog> dogCollection;
+
+        public ObservableCollection<Dog> DogCollection
+        {
+            get { return dogCollection; }
+            set { dogCollection = value; OnPropertyChanged(); }
+        }
+
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { isBusy = value; OnPropertyChanged(); }
+        }
+
+        public async Task LoadData()
+        {
+            IsBusy = true;
+            dogList = await DogApiService.GetDogs();
+            DogCollection = new ObservableCollection<Dog>(dogList);
+            IsBusy = false;
+
+        }
+    }
+}
